5e37ea8f2ce9e9594a575c508edcc9b8fcb2238f
Joaquin Musanti <joaquinmusanti@gmail.com>
2025-08-09 18:39:48 -0300

    rebase

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/mobybots-martech">
         <Header />
         <Routes>
           <Route path="/" element={<Index />} />
